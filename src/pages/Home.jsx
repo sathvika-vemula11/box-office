25ce46666e711def0794ba8319557112802cd414
sathvika <sathvikavemula2002@gmail.com>
2022-08-17 22:05:59 +0530

    created search bar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainPage from '../components/MainPage';
 
 function Home() {
-  return <MainPage>helloooooooo</MainPage>;
+  const [input, setInput] = useState('');
+  const onInputChange = ev => {
+    setInput(ev.target.value);
+  };
+
+  const onSearch = () => {
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+      .then(r => r.json())
+      .then(result => {
+        console.log(result);
+      });
+  };
+
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) {
+      onSearch();
+    }
+  };
+
+  return (
+    <MainPage>
+      <input
+        type="text"
+        onChange={onInputChange}
+        value={input}
+        onKeyDown={onKeyDown}
+      />
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+    </MainPage>
+  );
 }
 
 export default Home;
